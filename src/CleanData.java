5a2dc1cdaef04fab4d32c197c310e921f282a3ba
amedtamimi <31901003008@std.bau.edu.jo>
2021-11-10 12:13:22 +0200

    new Update

--- src/CleanData.java
+++ src/CleanData.java
@@ -80,9 +80,75 @@ public class CleanData {
             System.out.println("The Final result");
             System.out.println(result);
         }
+        else{
+            System.out.println("How many split you want?");
+            for ( int i=1; i <n ; i++) {
+                if(n%i==0){
+                    sp.add(i);
+                }
+            }
+            System.out.println(sp);
+            int spl = Reader.nextInt();
+            ArrayList<Integer>[] d = (ArrayList<Integer>[]) new ArrayList[spl];
+            for (int i = 0; i <spl ; i++) {
+                d[i] = new ArrayList<>();
+            }
+            int counter = 0;
+            for (int i = 0; i <spl ; i++) {
+                for ( int j =0; j <n/spl ; j++) {
+                    d[i].add(arr.get(counter));
+                    counter++;
+                }
+            }
+            System.out.println(Arrays.toString(d));
+            System.out.println("Chose the method");
+            System.out.println("1-Mean \n2-Median \n3-highOrLow algo \nChose 1 oR 2 oR 3");
+            int algo = Reader.nextInt();
+            switch (algo){
+                case 1: {
+                    for (int i = 0; i <spl ; i++) {
+                        int val = mean(d[i]);
+                        for ( int j =0; j <n/spl ; j++) {
+                            d[i].set(j,val);
+                        }
+                    }
+                    System.out.println(Arrays.toString(d));
+                }break;
+                case 2:{
+
+                    for (int i = 0; i <spl ; i++) {
+                        int val = median(d[i]);
+                        for ( int j =0; j <n/spl ; j++) {
+                            d[i].set(j,val);
+                        }
+                    }
+                    System.out.println(Arrays.toString(d));
+                }break;
+                case 3:{
+                    for (int i = 0; i <spl ; i++) {
+                        for ( int j =0; j <n/spl ; j++) {
+                            highAndLow(d[i]);
+                        }
+                    }
+                    System.out.println(Arrays.toString(d));
+                }break;
+                default:
+                    System.out.println("Error in Enter Data");
+            }
+            List<Integer>result = new ArrayList<>();
+            int counter1 = 0;
+            for (int i = 0; i <spl ; i++) {
+                for ( int j =0; j <n/spl ; j++) {
+                    result.add(d[i].get(j));
+                }
+            }
+            System.out.println("The Final result");
+            System.out.println(result);
+
+        }
 
     }
-    static int median(List<Integer>arr){
+    static int mean(List<Integer>arr){
         int avg =0;
         int sum =0;
         for (int i = 0; i <arr.size() ; i++) {
@@ -91,7 +157,7 @@ public class CleanData {
         avg = sum/arr.size();
         return avg;
     }
-    static int mean(List<Integer>arr){
+    static int median(List<Integer>arr){
         int first = 0;
         int second = 0;
         int mean = 0;
